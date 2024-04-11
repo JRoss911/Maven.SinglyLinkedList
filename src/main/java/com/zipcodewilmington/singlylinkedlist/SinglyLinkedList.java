7b377d56e11ce5730e0af63d966c6cc66b7a8fd7
Jorden Ross <JRoss9117@gmail.com>
2024-04-11 13:16:40 -0400

    Still working on it

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,108 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    private Node head; //Head is first node in linked list
+    private int size; //Size changes as elements/data is added and removed
+
+    public boolean isEmpty() {
+        SinglyLinkedList list = new SinglyLinkedList();
+        if (list.length() == 0 )
+            return true;
+        return false;
+    }
+//https://javarevisited.blogspot.com
+
+// Node is nested class because it only exists along with linked list
+// Node is private because it's implementation detail
+
+    public void append(String data) {
+        if(head == null){
+            head = new Node(data);
+            return;
+        }
+        tail().next = new Node(data);
+    }
+    private Node tail () {
+        Node tail = head;
+
+        //Find last element of linked list known as tail
+        while(tail.next != null) {
+            tail = tail.next;
+        }
+        return tail;
+
+    }
+    public int length() {
+        int length = 0; Node current = head;
+        // Starts counting from head - first node
+        while(current != null) {
+            length++;
+            current = current.next;
+        }
+        return length;
+    }
+
+    public void addFirst(String data) {
+        Node node = new Node(data);
+        if (head == null) {
+            head = node;
+            head.next = null;
+        }
+        node.next = head;
+        head = node;
+    }
+
+    public void addLast(String data) {
+        Node node = new Node(data);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node currentNode = head;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = node;
+
+    }
+
+    public String addElement(String elementToAdd) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        Node newNode = new Node(elementToAdd);
+        newNode.setNext(head);
+        head = newNode;
+        size++;
+        return list.addElement(elementToAdd);
+    }
+
+    public String remove(String elementToRemove) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        Node newNode = new Node(elementToRemove);
+        newNode.setNext(head);
+        head = newNode;
+        size--;
+        return list.remove(elementToRemove);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private static class Node {
+        private Node next;
+        private String data;
+
+        public Node (String data){
+            this.data = data;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,84 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+//add -- add an element to the list
+//remove -- remove an element (specified by numeric index) from the list
+//contains -- returns true if the element is in the list, false otherwise
+//find -- returns the element's index if it is in the list, -1 otherwise
+//size -- returns the current size of the list
+//get -- returns the element at the specified index
+//copy -- returns a new linked list containing the same values (look up deep versus shallow copy)
+//sort -- sorts the list using your algorithm of choice. You must perform the sorting yourself (no fair using someone else's library)
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test public void testNewLinkedList() {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        assertTrue(list.isEmpty()); // linked list should be empty
+        assertEquals(0, list.length());
+        // length of linked list should be zero
+
+        // length of linked list should be 1
+        list.append("ABC");
+        assertTrue(list.isEmpty());
+        assertEquals(1, list.length());
+        // linked list should not be empty assertEquals(1, singly.length());
+    }
+
+    @Test
+    public void testAddElement(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        String elementToAdd = "D";
+
+        List<String> actual = Collections.singletonList("ABC" + elementToAdd);
+        List<String> expected = Collections.singletonList("ABC" + "D");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemoveElement(){
+    }
+
+    @Test
+    public void containsElement(){
+
+    }
+
+    @Test
+    public void findElement(){
+
+    }
+
+    @Test
+    public void sizeOfList(){
+
+    }
+
+    @Test
+    public void getElement(){
+
+    }
+
+    @Test
+    public void copyList(){
+
+    }
+
+    @Test
+    public void sortList(){
+
+    }
 }
